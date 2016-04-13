da6dbd46a3d588aa7244ca9b506ea508c93e5922
Andreas Johansson <and.johansson93@gmail.com>
2016-04-13 12:29:19 +0200

    Restructured file system for servo_calibration sketch. Must be places in folder of same name.

---
+++ util/servo_calibration/servo_calibration.ino
@@ -0,0 +1,19 @@
+/*
+  Script used to calibrate the wheel servos.
+*/
+
+#include <Servo.h>
+
+Servo servo;
+
+const byte servo_pin = 13;
+int calibration_speed = 1500;
+
+void setup() {
+ servo.attach(servo_pin);
+}
+
+void loop() {
+ servo.write(calibration_speed);
+ delay(20);
+}
